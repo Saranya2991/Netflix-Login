acfe693c4c39e792650284db0fafd372f0130660
Saranya2991 <saara2991@gmail.com>
2025-12-08 01:02:38 +0000

    adjust responsive design

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -78,32 +78,32 @@ function Login(){
         
         <div className="bg-black text-white p-4 m-8 mt-0 bg-opacity-80 rounded w-full mx-auto sm:w-full md:mx-auto md:w-3/5 lg:w-2/5 xl:w-2/5">
            
-           {fail && <p className="text-3xl font-bold text-black p-3 w-[354px] bg-yellow-500 rounded ml-4 sm:ml-24 md:ml-5 lg:ml-12 ">{fail}</p>}
-           <h1 className="text-white text-3xl font-bold mr-64 lg:mr-60">Sign In</h1>
+           {fail && <p className="text-3xl font-bold text-black p-3 w-[354] md:w-[354px] bg-yellow-500 rounded ml-4 sm:ml-24 md:ml-5 lg:ml-12 ">{fail}</p>}
+           <h1 className="text-white text-3xl font-bold mr-32 sm:mr-64 lg:mr-60">Sign In</h1>
                 <div>
-                <input onChange={handleuser} className="p-3 mt-6 pr-36 bg-black bg-opacity-70 border border-gray-500 rounded font-semibold" type="email" placeholder="Email address or number"></input><br></br>
+                <input onChange={handleuser} className="p-3 mt-6 pr-30 sm:pr-36 bg-black bg-opacity-70 border border-gray-500 rounded font-semibold" type="email" placeholder="Email address or number"></input><br></br>
                 {uerror && <p className='text-red-500 border-red-500'>{uerror}</p>}
-                <input onChange={handlepass} className="p-3 mt-4 pr-36 bg-black bg-opacity-70 border border-gray-500 rounded font-semibold" type="password" placeholder="Password"></input><br></br>
+                <input onChange={handlepass} className="p-3 mt-4 pr-30 sm:pr-36 bg-black bg-opacity-70 border border-gray-500 rounded font-semibold" type="password" placeholder="Password"></input><br></br>
                 {perror && <p className='text-red-500'>{perror}</p>}
-                <button onClick={handlesubmit} className="p-2 mt-4 font-bold bg-red-600 w-[352px] rounded hover:bg-red-700">Sign In</button>
+                <button onClick={handlesubmit} className="p-2 mt-4 font-bold bg-red-600 w-[220px] sm:w-[352px] rounded hover:bg-red-700">Sign In</button>
                 
                 <p className="p-2 mt-1 font-semibold text-gray-400">OR</p>
-                <button className="p-2 mt-1 font-bold bg-gray-50 bg-opacity-20 w-[352px] rounded hover:bg-opacity-15">Use a sign-in code</button><br></br>
+                <button className="p-2 mt-1 font-bold bg-gray-50 bg-opacity-20 w-[220px] sm:w-[352px] rounded hover:bg-opacity-15">Use a sign-in code</button><br></br>
                 
                 <div>
-                <p className="mt-2 mb-0 text-lg underline hover:opacity-50 lg:ml-[8px]"><a href="#">Forgot password?</a></p><br></br>
+                <p className="mt-2 mb-0 text-lg underline hover:opacity-50 ml-2 lg:ml-[8px]"><a href="#">Forgot password?</a></p><br></br>
                 </div>
                 </div>
-                <div className="flex flex-col justify-start leading-4 mt-0 w-full">
-                <div className="ml-5 sm:ml-24 md:ml-6 lg:ml-[5px] xl:ml-[55px] text-lg flex items-center w-full">
+                <div className="flex flex-col justify-start leading-4 mt-0 w-full text-sm">
+                <div className="ml-7 sm:ml-24 md:ml-6 lg:ml-[5px] xl:ml-[55px] text-sm sm:text-lg flex items-center w-full">
                     <input className="w-5 h-5" type="checkbox"></input>
                     <label className="ml-2">Remember me.</label>
                  </div><br></br>
-                <p className="text-gray-300 mr-36 sm:mr-36 md:mr-[135px] lg:mr-[136px]">New to Netflix?<a className="font-bold text-white hover:underline" href="#">Sign up now.</a></p><br></br>
+                <p className="text-gray-300 mr-7 sm:mr-44 md:mr-[160px] lg:mr-[155px]">New to Netflix?<a className="font-bold text-white hover:underline" href="#">Sign up now.</a></p><br></br>
                 </div>
                 <div className="flex flex-col justify-start w-full text-sm/3">
-                <p className="text-gray-400 text-md sm:tracking-widest md:tracking-normal lg:tracking-normal xl:tracking-wide ml-4 sm:ml-24  md:ml-6 lg:ml-[8px] xl:ml-[56px] xl:text-sm text-left md:text-left leading-4">This page is protected by Google reCAPTCHA to ensure you're not a bot.</p><br></br>
-                <p className="mr-[278px] text-blue-700 text-sm font-semibold underline md:mr-[265px] lg:mr-68 xl:ml-[5px]"><a href="">Learn more.</a></p>
+                <p className="text-gray-400 text-xs sm:text-sm tracking-tighter sm:tracking-widest md:tracking-normal lg:tracking-normal xl:tracking-wide ml-7 sm:ml-24  md:ml-6 lg:ml-[8px] xl:ml-[56px] xl:text-sm text-left md:text-left leading-4">This page is protected by Google reCAPTCHA to ensure you're not a bot.</p><br></br>
+                <p className="mr-[130px] sm:mr-[278px] text-blue-700 text-sm font-semibold underline md:mr-[265px] lg:mr-68 xl:ml-[5px]"><a href="">Learn more.</a></p>
             </div>
         </div>
         </div>
